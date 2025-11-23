e355b49952d40cbdf3140cff561e508ce97f6730
Repo Specific Name <repo@example.com>
2025-11-23 15:38:25 +0545

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 const Home = () => {
   return (
-    <div
-      className="min-h-screen flex items-center justify-center text-4xl text-white 
-                 bg-[url('https://assets.designtemplate.io/Rustic%20Blue%20Metal%20Background%20Image%20700.webp')] 
-                 bg-cover bg-center"
-    >
-      Home page
+    <div className='max-w-7xl justify-between items-center mt-30 ml-65'>
+      <img className='h-150 rounded-4xl shadow-2xl' src="https://images.unsplash.com/photo-1664455340023-214c33a9d0bd?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
